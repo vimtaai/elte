8dba2444c86a2fea8093ea08e3b8081cedf577fc
Visnovitz Márton <vimtaai@INF.ELTE.HU>
2017-03-14 12:57:35 +0100

    2017.03.14/1

---
+++ 2016-17-2/mt2/gyak05/script.js
@@ -0,0 +1,30 @@
+// Segédfüggvények
+const $ = document.querySelector.bind(document);
+
+// Állapottér
+let store = {
+    state: {
+
+    },
+    actions: {
+
+    }
+};
+
+// Komponensek
+function Game(board) {
+    return board.map(row => Row(row)).join('');
+}
+
+function Row(row) {
+    return `<tr>${row.map(cell => Cell(cell)).join('')}</tr>`;
+}
+
+function Cell(cell) {
+    return `<td><button>a</button></td>`;
+}
+
+// Ikonok
+// Bomba: 💣
+// Zászló: 🚩
+// Kérdőjel: ❔
