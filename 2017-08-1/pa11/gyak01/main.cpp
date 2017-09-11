f9825673ac53cb852ed5a47247d1d5611d2e9570
Visnovitz Márton <vimtaai@INF.ELTE.HU>
2017-09-11 11:57:13 +0200

    2017.09.11

---
+++ 2017-08-1/pa11/gyak01/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int szam = 42;
+    int a, b;
+    cout << "Hello world!" << endl;
+    cout << "A szamom: " << szam << endl;
+    char betu = 'a';
+    cout << szam + betu << endl;
+    return 0;
+}
