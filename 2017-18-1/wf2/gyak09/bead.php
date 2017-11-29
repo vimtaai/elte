33e34a832a559b29cc664c6b9044c7542727a1e9
Visnovitz Márton <vimtaai@INF.ELTE.HU>
2017-11-29 17:33:12 +0100

    2017.11.29./2

--- 2017-18-1/wf2/gyak09/bead.php
+++ 2017-18-1/wf2/gyak09/bead.php
@@ -1,63 +1,81 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title></title>
-  <link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.min.css">
-</head>
-<body>
-  <nav class="navbar navbar-expand navbar-dark bg-primary">
-    <a class="navbar-brand" href="index.php">Beadandó-kezelő</a>
-    <ul class="navbar-nav">
-      <li class="nav-item active"><a class="nav-link" href="index.php">Feladatok</a></li>
-      <li class="nav-item"><a class="nav-link" href="feladatlista.php">Feltöltött fájlok</a></li>
-    </ul>
-  </nav>
+<?php
 
-  <main class="container">
-    <ol class="breadcrumb mt-2">
-      <li class="breadcrumb-item"><a href="index.php">Feladatok</a></li>
-      <li class="breadcrumb-item"><a href="feladatlista.php">Feladat beadása (PHP beadandó)</a></li>
-    </ol>
+const MAX_SIZE = 4194304;
 
-    <h3 class="py-2">Feladat beadása</h3>
+$errors = [];
+$messages = [];
 
-    <form method="post" action="bead.php">
-      <fieldset>
-        <legend>PHP beadandó</legend>
-        <section class="row">
-          <div class="col-sm-6">
-            <div class="row">
-              <div class="form-group col-md-8 col-sm-12">
-                <label for="neptun">Neptun kód</label>
-                <input type="text" class="form-control col-md-6 col-sm-8" id="neptun" pattern="[0-9a-zA-Z]{6}" required>
-                <small class="form-text text-muted">Add meg a Neptun kódodat, hogy be lehessen azonoístani a beadandódat.</small>
-              </div>
-              <div class="form-group col-md-8 col-sm-12">
-                <label for="fajl">Feltöltendő fájl</label>
-                <input type="file" class="form-control col-sm-12" id="fajl" required>
-                <small class="form-text text-muted">A feltöltendő fájl mérete nem haladhatja meg a 4 MB-ot.</small>
-              </div>
-              <div class="form-group col-md-8 col-sm-12">
-                <input type="submit" class="btn btn-lg btn-success" value="Feltöltés">
-              </div>
+if (count($_FILES) > 0 && count($_POST) > 0) {
+  // Hibaellenőrzés
+  if ($_FILES['fajl']['size'] > MAX_SIZE) {
+    $errors[] = 'Túl nagy a fájlméret.';
+  }
+  if ($_FILES['fajl']['error'] !== 0) {
+    $errors[] = 'Hiba a fájlfeltöltésben.';
+  }
+  if (count($errors) == 0) {
+    // A fájl elmentése
+    $folder = 'files/';
+    $user = strtolower($_POST['neptun']);
+    $timestamp = date('Y-m-d-H-i-s');
+    $name = $_FILES['fajl']['name'];
+    $fileName =  $folder . $user . '-' . $timestamp . '-' . $name;
+    if (move_uploaded_file($_FILES['fajl']['tmp_name'], $fileName)) {
+      $messages[] = 'Sikeres feltöltés.';
+    } else {
+      $error[] = 'Hiba a fájl mentésekor.';
+    }
+  }
+}
+
+?>
+<?php include './includes/header.php'; ?>
+<main class="container">
+  <ol class="breadcrumb mt-2">
+    <li class="breadcrumb-item"><a href="index.php">Feladatok</a></li>
+    <li class="breadcrumb-item"><a href="feladatlista.php">Feladat beadása (PHP beadandó)</a></li>
+  </ol>
+
+  <h3 class="py-2">Feladat beadása</h3>
+
+  <form method="post" action="bead.php" enctype="multipart/form-data">
+    <fieldset>
+      <legend>PHP beadandó</legend>
+      <section class="row">
+        <div class="col-sm-6">
+          <div class="row">
+            <div class="form-group col-md-8 col-sm-12">
+              <label for="neptun">Neptun kód</label>
+              <input name="neptun" type="text" class="form-control col-md-6 col-sm-8" id="neptun" pattern="[0-9a-zA-Z]{6}" required>
+              <small class="form-text text-muted">Add meg a Neptun kódodat, hogy be lehessen azonoístani a beadandódat.</small>
+            </div>
+            <div class="form-group col-md-8 col-sm-12">
+              <label for="fajl">Feltöltendő fájl</label>
+              <input name="fajl" type="file" class="form-control col-sm-12" id="fajl" required>
+              <small class="form-text text-muted">A feltöltendő fájl mérete nem haladhatja meg a 4 MB-ot.</small>
+            </div>
+            <div class="form-group col-md-8 col-sm-12">
+              <input type="submit" class="btn btn-lg btn-success" value="Feltöltés">
             </div>
           </div>
-          <div class="col-sm-6">
-            <h4>Mit kell beadni?</h4>
-            <p>
-              Beadni egy darab fájt kell. Amennyiben a megoldás több fájlból áll, akkor azt egy tömörített állomány formájában kell beadni.
-            </p>
-          </div>
-        </section>
-      </fieldset>
-    </form>
+        </div>
+        <div class="col-sm-6">
+          <h4>Mit kell beadni?</h4>
+          <p>
+            Beadni egy darab fájt kell. Amennyiben a megoldás több fájlból áll, akkor azt egy tömörített állomány formájában kell beadni.
+          </p>
+        </div>
+      </section>
+    </fieldset>
+  </form>
 
-    <!-- Ide jönnek majd az üzenetek -->
-    
-    <!-- Üzenetek vége -->
-  </main>
-</body>
-</html>
+  <!-- Ide jönnek majd az üzenetek -->
+  <?php foreach ($messages as $message): ?>
+    <div class="alert alert-success"><?= $message ?></div>
+  <?php endforeach; ?>
+  <?php foreach ($errors as $error): ?>
+    <div class="alert alert-danger"><?= $error ?></div>
+  <?php endforeach; ?>
+  <!-- Üzenetek vége -->
+</main>
+<?php include './includes/footer.php'; ?>
--- 2017-18-1/wf2/gyak09/fajllista.php
+++ 2017-18-1/wf2/gyak09/fajllista.php
@@ -1,69 +1,51 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title></title>
-  <link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.min.css">
-</head>
-<body>
-  <nav class="navbar navbar-expand navbar-dark bg-primary">
-    <a class="navbar-brand" href="index.php">Beadandó-kezelő</a>
-    <ul class="navbar-nav">
-      <li class="nav-item"><a class="nav-link" href="index.php">Feladatok</a></li>
-      <li class="nav-item active"><a class="nav-link" href="feladatlista.php">Feltöltött fájlok</a></li>
-    </ul>
-  </nav>
+<?php include './includes/header.php'; ?>
+<main class="container">
+  <ol class="breadcrumb mt-2">
+    <li class="breadcrumb-item"><a href="feladatlista.php">Feltöltött fájlok</a></li>
+    <li class="breadcrumb-item"><a href="fajllista.php">JavaScript beadandó (DOM)</a></li>
+  </ol>
 
-  <main class="container">
-    <ol class="breadcrumb mt-2">
-      <li class="breadcrumb-item"><a href="feladatlista.php">Feltöltött fájlok</a></li>
-      <li class="breadcrumb-item"><a href="fajllista.php">JavaScript beadandó (DOM)</a></li>
-    </ol>
+  <h3 class="py-2">JavaScript beadandó (DOM)</h3>
 
-    <h3 class="py-2">JavaScript beadandó (DOM)</h3>
+  <table class="table table-striped table-hover table-bordered">
+    <thead class="thead-dark">
+      <tr>
+        <th>Feltöltő</th>
+        <th>Fájl neve</th>
+        <th>Fájl mérete</th>
+        <th>Feltöltés ideje</th>
+        <th>Letöltés</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td colspan="5">
+          <a href="feladatlista.php">Vissza a feladatokhoz</a>
+        </td>
+      </tr>
 
-    <table class="table table-striped table-hover table-bordered">
-      <thead class="thead-dark">
-        <tr>
-          <th>Feltöltő</th>
-          <th>Fájl neve</th>
-          <th>Fájl mérete</th>
-          <th>Feltöltés ideje</th>
-          <th>Letöltés</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td colspan="5">
-            <a href="feladatlista.php">Vissza a feladatokhoz</a>
-          </td>
-        </tr>
+      <!-- Feldat eleje -->
+      <tr>
+        <td>WP1C0X</td>
+        <td>wp1c0x.zip</td>
+        <td>14.2 kB</td>
+        <td>2017.11.29. 14:15</td>
+        <td>
+          <a href="#" class="btn btn-secondary btn-sm btn-block">Letöltés</a>
+        </td>
+      </tr>
+      <!-- Feldat vége -->
 
-        <!-- Feldat eleje -->
-        <tr>
-          <td>WP1C0X</td>
-          <td>wp1c0x.zip</td>
-          <td>14.2 kB</td>
-          <td>2017.11.29. 14:15</td>
-          <td>
-            <a href="#" class="btn btn-secondary btn-sm btn-block">Letöltés</a>
-          </td>
-        </tr>
-        <!-- Feldat vége -->
-
-        <tr>
-          <td>WP1C0X</td>
-          <td>wp1c0x_2.zip</td>
-          <td>14.2 kB</td>
-          <td>2017.12.01. 13:37</td>
-          <td>
-            <a href="#" class="btn btn-secondary btn-sm btn-block">Letöltés</a>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </main>
-</body>
-</html>
+      <tr>
+        <td>WP1C0X</td>
+        <td>wp1c0x_2.zip</td>
+        <td>14.2 kB</td>
+        <td>2017.12.01. 13:37</td>
+        <td>
+          <a href="#" class="btn btn-secondary btn-sm btn-block">Letöltés</a>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</main>
+<?php include './includes/footer.php'; ?>
