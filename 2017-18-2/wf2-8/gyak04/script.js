65b82e0a6b892e1300bed07e4919ee2ba4d3b5c5
Visnovitz Márton <vimtaai@INF.ELTE.HU>
2018-03-06 17:31:51 +0100

    2018.03.06/3

--- 2017-18-2/wf2-8/gyak04/script.js
+++ 2017-18-2/wf2-8/gyak04/script.js
@@ -1,19 +1,68 @@
 // Segédfüggvények
 const $ = (s) => document.querySelector(s);
 
+function delegate(parent, children, type, handler) {
+  function delegatedFunction(event) {
+    let target = event.target;
+    if (target.matches(`${parent} ${children}, ${parent} ${children} *`)) {
+      while (!target.matches(children)) {
+        target = targer.parentNode;
+      }
+
+      // event.delegatedTarget = target;
+      // handler(event);
+      return handler.call(target, event);
+    }
+  }
+  $(parent).addEventListener(type, delegatedFunction);
+}
+
 // Állapot
-let state;
+let game;
+let isStarted;
+let maxX, maxY;
+let mineCount;
+
+// Aknák lehelyezése
+function placeMines(clickX, clickY) {
+  for (let i = 0; i < mineCount; ++i) {
+    let rx;
+    let ry;
+    do {
+      rx = Math.trunc(Math.random() * maxX);
+      ry = Math.trunc(Math.random() * maxY);
+    } while (game[ry][rx].isMine || 
+             (rx == clickX && ry == clickY));
+
+    game[ry][rx].isMine = true;
+    for (let xi = -1; xi <= 1; ++xi) {
+      for (let yi = -1; yi <= 1; ++yi) {
+        if (rx + xi >= 0 &&
+            rx + xi <  maxX &&
+            ry + yi >= 0 &&
+            ry + yi <  maxY) {
+          game[ry + yi][rx + xi].neighborCount++;
+        }
+      }
+    }
+  }
+}
 
 // Kezdőállapot létrehozása
 function init() {
   const x = $('#x').value;
   const y = $('#y').value;
   const m = $('#m').value;
-  state = [];
+  // Kezdőállapot
+  game = [];
+  isStarted = false;
+  maxX = x;
+  maxY = y;
+  mineCount = m;
   for (let i = 0; i < y; ++i) {
-    state[i] = [];
+    game[i] = [];
     for (let j = 0; j < x; ++j) {
-      state[i][j] = {
+      game[i][j] = {
         isRevealed: false,
         isMine: false,
         isFlagged: false,
@@ -21,26 +70,7 @@ function init() {
       };
     }
   }
-  for (let i = 0; i < m; ++i) {
-    let rx;
-    let ry;
-    do {
-      rx = Math.trunc(Math.random() * x);
-      ry = Math.trunc(Math.random() * y);
-    } while (state[ry][rx].isMine);
-    state[ry][rx].isMine = true;
-    for (let xi = -1; xi <= 1; ++xi) {
-      for (let yi = -1; yi <= 1; ++yi) {
-        if (rx + xi >= 0 &&
-            rx + xi <  x &&
-            ry + yi >= 0 &&
-            ry + yi <  y) {
-          state[ry + yi][rx + xi].neighborCount++;
-        }
-      }
-    }
-  }
-  render(state);
+  render();
 }
 $('input[type=button]').addEventListener('click', init);
 
@@ -50,9 +80,40 @@ function next() { }
 // Játék végének ellenőrzése
 function end() { }
 
+// Bal kattintás
+function leftClick() {
+  //console.log(this);
+  const td = this.parentNode;
+  const tr = td.parentNode;
+  const x = td.cellIndex;
+  const y = tr.rowIndex;
+  if (!isStarted) {
+    placeMines(x, y);
+    isStarted = true;
+  }
+  if (!game[y][x].isFlagged) {
+    game[y][x].isRevealed = true;
+    render();
+  }
+}
+delegate('table', 'button', 'click', leftClick);
+
+// Jobb kattintás
+function rightClick(event) {
+  console.log(this);
+  event.preventDefault();
+  const td = this.parentNode;
+  const tr = td.parentNode;
+  const x = td.cellIndex;
+  const y = tr.rowIndex;
+  game[y][x].isFlagged = !game[y][x].isFlagged;
+  render();
+}
+delegate('table', 'button', 'contextmenu', rightClick);
+
 // Állapot megjelenítése
-function render(state) {
-  $('table').innerHTML = genTable(state);
+function render() {
+  $('table').innerHTML = genTable(game);
 }
 
 // Táblázat generálása
