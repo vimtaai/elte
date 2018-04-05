896a403537b33115c5ae878f9805ff544a4f8f11
Visnovitz Márton <vimtaai@INF.ELTE.HU>
2018-04-05 18:41:44 +0200

    2018.04.05/2

---
+++ 2017-18-2/aat2/szamsor_tomorit/szamsor_tomorit/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szamsor_tomorit
+{
+    class Program
+    {
+        static string Tomorit(int szam, int db)
+        {
+            if (db > 4)
+            {
+                return "/" + db + "/" + szam;
+            }
+            else
+            {
+                string eredmeny = "";
+                for (int i = 0; i < db; i++)
+                {
+                    eredmeny += szam;
+                }
+                return eredmeny;
+            }
+        }
+        static void Main(string[] args)
+        {
+            // Beolvasás
+            string szamsor;
+            Console.Write("Add meg a tömörítendő számsort: ");
+            szamsor = Console.ReadLine();
+            // Önellenőrzés
+            Console.WriteLine(Tomorit(1, 5));
+            Console.WriteLine(Tomorit(4, 3));
+            Console.WriteLine(Tomorit(9, 6));
+            Console.WriteLine(Tomorit(6, 0));
+            // Feldolgozás
+            // Kiírás
+        }
+    }
+}
---
+++ 2017-18-2/aat2/zh_feladat/zh_feladat/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zh_feladat
+{
+    class Program
+    {
+        static bool Oho3(int x)
+        {
+            return x % 3 == 0;
+        }
+
+        static void Main(string[] args)
+        {
+            int kezdoertek, osszeg, n, d;
+            // Számtani sorozat
+            Random random = new Random();
+            kezdoertek = random.Next(-10, 11);
+            do
+            {
+                Console.Write("Add meg a sorozat elemszámát [1-1000]: ");
+                n = Convert.ToInt32(Console.ReadLine());
+                if (n <= 0 || n > 1000)
+                {
+                    Console.WriteLine("Hibás bemenet! Próbáld újra!");
+                }
+            } while (n <= 0 || n > 1000);
+            Console.Write("Add meg a sorozat differenciáját: ");
+            d = Convert.ToInt32(Console.ReadLine());
+            List<int> sorozat = new List<int>();
+            sorozat.Add(kezdoertek);
+            for (int i = 1; i <= n - 1; i++)
+            {
+                //int ujelem = sorozat[i - 1] + d;
+                int ujelem = sorozat.Last() + d;
+                sorozat.Add(ujelem);
+            }
+            // Önellenőrzés
+            //for (int i = 0; i < n; i++)
+            //for (int i = 0; i < sorozat.Count; i++)
+            //{
+            //    Console.Write(sorozat[i] + " ");
+            //}
+            // Tételalkalmazás
+            osszeg = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (Oho3(sorozat[i]))
+                {
+                    osszeg += sorozat[i];
+                }
+            }
+
+            //foreach (int elem in sorozat)
+            //{
+            //    if (elem % 3 == 0)
+            //    {
+            //        osszeg += elem;
+            //    }
+            //}
+            Console.WriteLine("A sorozatban a hárommal osztható számok összege: " + osszeg);
+        }
+    }
+}
