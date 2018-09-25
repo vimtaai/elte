c087bdeaddd0f6ae3f60f9c626ee6c24e47c9919
Márton Visnovitz <vimtaai@inf.elte.hu>
2018-09-25 10:03:22 +0200

    2018.09.25/2

---
+++ 2018-19-1/wf2/gyak03-todolist/script.js
@@ -0,0 +1,57 @@
+/* SEGÉDFÜGGVÉNYEK */
+
+function $ (selector) {
+  return document.querySelector(selector);
+}
+
+function getTodoList() {
+  return JSON.parse(localStorage.todoList || '[]');
+}
+
+function setTodoList(todoList) {
+  return localStorage.todoList = JSON.stringify(todoList);
+}
+
+// const $ = s => document.querySelector(s);
+
+/* ÁLLAPOTTÉR */
+
+// const todoList = [];
+
+/* UI GENERÁTOROK */
+
+function genTodoList (todoList) {
+  let html = '';
+  for (const todo of todoList) {
+    html += `<li>${todo}</li>`;
+    // html += '<li>' + todo + '</li>';
+  }
+  return html;
+
+  // return todoList.map(todo => `<li>${todo}</li>`).join('');
+}
+
+/* ESEMÉNYKEZELŐK */
+function buttonClick () {
+  const todoList = getTodoList();
+
+  // console.log('rám kattintottak');
+  const todo = $('input').value;
+
+  if (todoList.includes(todo)) {
+    alert('Ilyen már van!');
+    return;
+  }
+
+  todoList.push(todo);
+  console.log(todoList);
+  $('ul').innerHTML = genTodoList(todoList);
+  $('input').value = '';
+  $('input').focus();
+
+  setTodoList(todoList);
+}
+$('button').addEventListener('click', buttonClick);
+
+/* PROGRAM INDUL */
+$('ul').innerHTML = genTodoList(getTodoList());
