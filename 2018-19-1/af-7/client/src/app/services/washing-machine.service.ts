1e83e7f6e17e301342322a5bd6e1b1c44d43d4ea
Márton Visnovitz <vimtaai@inf.elte.hu>
2018-11-19 11:10:44 +0100

    2018.11.19/1

--- 2018-19-1/af-7/client/src/app/services/washing-machine.service.ts
+++ 2018-19-1/af-7/client/src/app/services/washing-machine.service.ts
@@ -8,7 +8,7 @@ export class WashingMachineService {
   private _MACHINES: WashingMachine[] = [
     { id: 1, building: 'A', floor: 4, number: 1, outOfOrder: false } as WashingMachine,
     { id: 2, building: 'A', floor: 4, number: 2, outOfOrder: false } as WashingMachine,
-    { id: 3, building: 'A', floor: 8, number: 1, outOfOrder: false } as WashingMachine,
+    { id: 3, building: 'A', floor: 8, number: 1, outOfOrder: true } as WashingMachine,
     { id: 4, building: 'A', floor: 8, number: 2, outOfOrder: false } as WashingMachine
   ];
 
